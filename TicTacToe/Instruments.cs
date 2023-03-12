7edb418a33836a0e0200a91878e63969794f57db
Stepan <72158358+filippov-code@users.noreply.github.com>
2023-03-12 22:25:47 +0300

    init

---
+++ TicTacToe/Instruments.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+namespace TicTacToe
+{
+    public static class Instruments
+    {
+        public const char XSign = 'X';
+        public const char OSign = 'O';
+        public const char EmptySign = '.';
+        /// <summary>
+        /// Возвращает пустое поле в виде строки
+        /// </summary>
+        /// <returns></returns>
+        public static string GetEmptyField()
+        {
+            return string.Join("", Enumerable.Range(0, 9).Select(x => EmptySign));
+        }
+        /// <summary>
+        /// Конвертирует строковое представление игрового поля в таблицу символов
+        /// </summary>
+        /// <param name="field"></param>
+        /// <returns>Игровое поле в виде двумерного массива</returns>
+        public static char[,] ConvertStringToFieldTable(string field)
+        {
+            char[,] fieldChars = new char[3, 3];
+            for (int y = 0; y < 3; y++)
+                for (int x = 0; x < 3; x++)
+                    fieldChars[x, y] = field[y * 3 + x];
+
+            return fieldChars;
+        }
+        /// <summary>
+        /// Ковертирует таблицу игрового поля в строку
+        /// </summary>
+        /// <param name="fieldTable"></param>
+        /// <returns>Игровое поле в виде строки</returns>
+        public static string ConvertFieldTableToString(char[,] fieldTable)
+        {
+            StringBuilder sb = new();
+            for (int y = 0; y < 3; y++)
+                for (int x = 0; x < 3; x++)
+                    sb.Append(fieldTable[x, y]);
+
+            //foreach (char c in fieldTable)
+            //    sb.Append(c);
+
+            string result = sb.ToString();
+            return result;
+        }
+    }
+}
---
+++ TicTacToe/Logic.cs
@@ -0,0 +1,58 @@
+﻿
+namespace TicTacToe
+{
+    /// <summary>
+    /// Содержит логику игру в крестики нолики
+    /// </summary>
+    public static class Logic
+    {
+        /// <summary>
+        /// Проверяет есть ли победитель на игровом поле
+        /// </summary>
+        /// <param name="field"></param>
+        /// <returns></returns>
+        public static bool ThereIsAWinnerOnField(char[,] field)
+        {
+            //Check horisontals
+            for (int y = 0; y < 3; y++)
+            {
+                if (field[0, y] == Instruments.EmptySign)
+                    continue;
+                if (field[0, y] == field[1, y] && field[0, y] == field[2, y])
+                    return true;
+            }
+            //Check verticals
+            for (int x = 0; x < 3; x++)
+            {
+                if (field[x, 0] == Instruments.EmptySign)
+                    continue;
+                if (field[x, 0] == field[x, 1] && field[x, 0] == field[x, 2])
+                    return true;
+            }
+            //Check main diagonal
+            if (field[0, 0] != Instruments.EmptySign)
+            {
+                if (field[0, 0] == field[1, 1] && field[0, 0] == field[2, 2])
+                    return true;
+            }
+            //Check second diagonal
+            if (field[2, 0] != Instruments.EmptySign)
+            {
+                if (field[2, 0] == field[1, 1] && field[2, 0] == field[0, 2])
+                    return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// Проверяет возможен ли ход в заданной таблице игрового поля
+        /// </summary>
+        /// <param name="field"></param>
+        /// <returns></returns>
+        public static bool MoveHaveEndedOnField(char[,] field)
+        {
+            foreach (var sign in field)
+                if (sign == Instruments.EmptySign)
+                    return false;
+            return true;
+        }
+    }}
